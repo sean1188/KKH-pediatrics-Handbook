b2127966f8b9b78987dfc49b494060accae2ff6d
Seannn! <seanmac@Seannns-MacBook-Pro.local>
2017-03-26 01:17:38 +0800

    converting to swift

---
+++ KKH Paediatrics Anaesthesia Handbook - swift/KKH Paediatrics Anaesthesia Handbook - swift/extensions.swift
@@ -0,0 +1,19 @@
+//
+//  extensions.swift
+//  KKH Paediatrics Anaesthesia Handbook - swift
+//
+//  Created by Seannn! on 25/3/17.
+//  Copyright © 2017 SST inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+     func primaryColor() -> UIColor{
+        return .lightGray
+    }
+     func secondaryColor() -> UIColor{
+        return .lightGray
+    }
+}
