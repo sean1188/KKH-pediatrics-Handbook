e4780b4a45022a984fa51fd97534ac3950ac491a
Sean Lim <seanlimmac@Seans-MacBook-Pro.local>
2017-04-18 15:58:21 +0800

    build

--- KKH Paediatrics Anaesthesia Handbook legacy version/Pods/Target Support Files/Pods-KKH app/Pods-KKH app-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KKH_app : NSObject
-@end
-@implementation PodsDummy_Pods_KKH_app
-@end
--- KKH Paediatrics Anaesthesia Handbook legacy version/Pods/Target Support Files/Pods-KKH appUITests/Pods-KKH appUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KKH_appUITests : NSObject
-@end
-@implementation PodsDummy_Pods_KKH_appUITests
-@end
