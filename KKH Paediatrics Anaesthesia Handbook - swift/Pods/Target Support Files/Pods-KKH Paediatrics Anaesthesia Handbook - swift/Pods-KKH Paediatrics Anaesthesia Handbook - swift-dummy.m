acd359bee1e63f215fd2283db7240d5dda8a11fa
sean <seanlimmac@Seans-MacBook-Pro.local>
2017-06-14 09:05:05 +0800

    refactor search schema to functional

--- KKH Paediatrics Anaesthesia Handbook - swift/Pods/Target Support Files/Pods-KKH Paediatrics Anaesthesia Handbook - swift/Pods-KKH Paediatrics Anaesthesia Handbook - swift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KKH_Paediatrics_Anaesthesia_Handbook___swift : NSObject
-@end
-@implementation PodsDummy_Pods_KKH_Paediatrics_Anaesthesia_Handbook___swift
-@end
--- KKH Paediatrics Anaesthesia Handbook - swift/Pods/Target Support Files/Pods-KKH Paediatrics Anaesthesia Handbook - swiftTests/Pods-KKH Paediatrics Anaesthesia Handbook - swiftTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_KKH_Paediatrics_Anaesthesia_Handbook___swiftTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_KKH_Paediatrics_Anaesthesia_Handbook___swiftTestsVersionString[];
-
